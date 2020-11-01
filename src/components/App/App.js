7b4c85c4dc76a7c97099a745cc154ff14492cac1
AwrenNuit <a.rennaker@yahoo.com>
2020-10-31 23:03:17 -0500

    set up context api & firebase database listener, render calculation history to DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,43 @@
+import { createContext, useEffect, useReducer } from "react";
+import { db } from "../../firebase";
+import CalculationHistory from "../CalculationHistory/CalculationHistory";
 import Calculator from "../Calculator/Calculator";
 import "./App.css";
 
+export const Context = createContext();
+
+const initialState = {
+  calculationHistory: [],
+};
+
+const contextReducer = (state, action) => {
+  switch (action.type) {
+    case `SET_HISTORY`:
+      return { calculationHistory: [action.payload, ...state.calculationHistory], };
+    case `PREP_HISTORY`:
+      const newArray = state.calculationHistory.reverse().slice(1).reverse();
+      return { calculationHistory: newArray, };
+    default:
+      return initialState;
+  }
+};
+
 export default function App() {
-  return <Calculator />;
+  const [state, dispatch] = useReducer(contextReducer, initialState);
+
+  useEffect(() => {
+    db.ref("/history")
+      .orderByKey()
+      .limitToLast(10)
+      .on("child_added", (snap) => {
+        dispatch({ type: `SET_HISTORY`, payload: snap.val() });
+      });
+  }, []);
+
+  return (
+    <Context.Provider value={{ state, dispatch }}>
+      <Calculator />
+      <CalculationHistory />
+    </Context.Provider>
+  );
 }
--- src/components/CalculationHistory/CalculationHistory.js
+++ src/components/CalculationHistory/CalculationHistory.js
@@ -1,7 +1,30 @@
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../App/App";
+
 export default function CalculationHistory() {
+  const { state, dispatch } = useContext(Context);
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    if (state.calculationHistory.length === 10) {
+      setHistory([...state.calculationHistory]);
+    }
+  }, [state]);
+
+  useEffect(() => {
+    if (history.length === 10) {
+      dispatch({ type: `PREP_HISTORY` });
+    }
+  }, [history]);
+
   return (
     <div id="history-container">
       <h3>HISTORY</h3>
+      <ul>
+        {history.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
